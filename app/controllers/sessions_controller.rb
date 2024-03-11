2ccf787b208d042a0fc3d39587b48d2292baeb23
Muazzam Gilani <muazzam.gilani@kellogg.northwestern.edu>
2024-03-11 01:16:22 +0000

    HW 4 Able to Login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,28 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ "email" => params["email"] })
+    if @user
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        # assign a cookie
+        session["user_id"] = @user["id"]
+        # login the user
+        flash["notice"] = "You've logged in."
+        redirect_to "/places"
+      else
+        flash["notice"] = "Unsuccessful login."
+        redirect_to "/sessions/new"
+      end
+    else
+      flash["notice"] = "Unsuccessful login."
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
+  flash["notice"] = "Goodbye."
+  session["user_id"] = nil
+  redirect_to "/sessions/new"
   end
 end
   
