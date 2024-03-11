d6ee1e270f242ab37005f6618cab97bff700621d
Muazzam Gilani <muazzam.gilani@kellogg.northwestern.edu>
2024-03-11 01:56:01 +0000

    All User Stories complete (except images) front end work left

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -7,7 +7,12 @@ class PlacesController < ApplicationController
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
-    @entries = Entry.where({ "place_id" => @place["id"] })
+    if @current_user != nil
+     @entries = Entry.where({ "place_id" => @place["id"], "user_id" => @current_user["id"]})
+    else
+      flash["notice"] = "Please login before viewing entries"
+      redirect_to "/login"
+    end
   end
 
   def new
